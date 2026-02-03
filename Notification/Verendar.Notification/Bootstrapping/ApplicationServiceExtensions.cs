43886f64ec5318289f4fffdcf58e77f021bddcc0
duynp <duynguyen04302004@gmail.com>
2026-02-03 23:35:40 +0700

    update email service

--- Notification/Verendar.Notification/Bootstrapping/ApplicationServiceExtensions.cs
+++ Notification/Verendar.Notification/Bootstrapping/ApplicationServiceExtensions.cs
@@ -5,8 +5,11 @@ using Verendar.Common.Shared;
 using Verendar.Notification.Application.Dtos.ESms;
 using Verendar.Notification.Application.Services.Interfaces;
 using Verendar.Notification.Domain.Repositories.Interfaces;
+using Verendar.Notification.Infrastructure.Configuration;
 using Verendar.Notification.Infrastructure.Data;
 using Verendar.Notification.Infrastructure.ExternalServices.ESms;
+using Verendar.Notification.Apis;
+using Verendar.Notification.Infrastructure.ExternalServices.Resend;
 using Verendar.Notification.Infrastructure.Repositories.Implements;
 using Verendar.ServiceDefaults;
 
@@ -23,13 +26,26 @@ public static class ApplicationServiceExtensions
         builder.AddPostgresDatabase<NotificationDbContext>(Const.NotificationDatabase);
 
         builder.Services.Configure<ESmsOptions>(builder.Configuration.GetSection("ESms"));
+        builder.Services.Configure<ResendOptions>(builder.Configuration.GetSection(ResendOptions.SectionName));
+
+        // Add Memory Cache for template caching
+        builder.Services.AddMemoryCache();
 
         builder.Services.AddHttpClient<IESmsService, ESmsService>()
             .AddPolicyHandler(GetRetryPolicy())
             .AddPolicyHandler(GetCircuitBreakerPolicy());
 
+        // Resend Email Service - Using HttpClient directly with Resend REST API
+        builder.Services.AddHttpClient<IResendEmailService, ResendEmailService>()
+            .AddPolicyHandler(GetRetryPolicy())
+            .AddPolicyHandler(GetCircuitBreakerPolicy());
+
+        builder.Services.AddSingleton<IEmailTemplateService, RazorEmailTemplateService>();
+
+        // Notification Channels
         builder.Services.AddScoped<INotificationChannel, SmsChannel>();
         builder.Services.AddScoped<INotificationChannel, ZaloChannel>();
+        builder.Services.AddScoped<INotificationChannel, EmailChannel>();
         builder.Services.AddScoped<IChannelFactory, ChannelFactory>();
 
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -42,6 +58,8 @@ public static class ApplicationServiceExtensions
         app.MapDefaultEndpoints();
 
         app.UseCommonService();
+
+        app.MapEmailApis();
 
         if (app.Environment.IsDevelopment())
         {
